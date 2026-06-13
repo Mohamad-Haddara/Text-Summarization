c0a5de88961dd67d44ef0f0a3514bb4281655d54
Mohamad <mohamad.haddara@outlook.com>
2026-06-13 10:17:55 +0300

    update pydantic model to  valdiate response data

--- project/app/models/pydantic.py
+++ project/app/models/pydantic.py
@@ -2,3 +2,8 @@ from pydantic import BaseModel
 
 class SummaryPayloadSchema(BaseModel):
     url: str
+    
+
+# SummaryResponseSchema inherits from SummaryPayloadSchema model, adding id field
+class SummaryResponseSchema(SummaryPayloadSchema):
+    id: int
