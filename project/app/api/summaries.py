8d474de5d0e8a0e3c5bb8e20028d6d536f1fdb89
Mohamad <mohamad.haddara@outlook.com>
2026-06-13 09:35:38 +0300

    Create POST Route

---
+++ project/app/api/summaries.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, HTTPException
+
+from app.api import crud
+from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
+
+
+router = APIRouter()
+
+
+# POST Router
+# Define a handler that expect a payload, payload: SummaryPayloadSchema, with a URL
+
+@router.post("/", response_model = SummaryResponseSchema, status_code = 201)
+async def create_summary(payload: SummaryPayloadSchema):
+    summary_id = await crud.post(payload)
+    
+    response_object = {
+        "id": summary_id,
+        "url": payload.url
+    }
+    
+    return response_object
