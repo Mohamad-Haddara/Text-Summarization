fffdcbcee17cb016932ad893b9762eddcd6ea20f
Mohamad <mohamad.haddara@outlook.com>
2026-06-14 21:11:57 +0300

    add get route

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,6 +1,6 @@
 from app.models.tortoise import TextSummary
 from app.models.pydantic import SummaryPayloadSchema
-from typing import Union
+from typing import Union, List
 
 async def post(payload: SummaryPayloadSchema) -> int:
     summary = TextSummary(
@@ -18,4 +18,8 @@ async def get(id: int) -> Union[dict, None]:
         return summary
     
     return None
+
+async def get_all() -> List:
+    summaries = await TextSummary.all().values()
+    return summaries
 
